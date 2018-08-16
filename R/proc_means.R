ca6770725a56cb118a613d85bb2370632857a3ec
Craig McGowan <mcgowan.cj@gmail.com>
2018-08-16 19:57:12 +0200

    Initial commit, including code for proc means replica

---
+++ R/proc_means.R
@@ -0,0 +1,104 @@
+#' Replica of SAS's PROC MEANS
+#'
+#' Descriptive statistics for continuous variables, with the option of 
+#' stratifying by a categorical variable.
+#'
+#' @param df A data frame or tibble.
+#' @param vars A character vector of numeric variables to generate descriptive 
+#'   statistics for. If the default (\code{NULL}), all variables are included,
+#'   except for any specified in \code{by}.
+#' @param by Discrete variable. Separate statistics will be produced for 
+#'   each level. Default \code{NULL} provides statistics for all observations.
+#' @param n Display number of rows with values. Default \code{TRUE}.
+#' @param mean Display mean value. Default \code{TRUE}.
+#' @param sd Display standard deviation. Default \code{TRUE}.
+#' @param min Display minimum value. Default \code{TRUE}.
+#' @param max Display maximum value. Default \code{TRUE}.
+#' @param median Display median value. Default \code{FALSE}.
+#' @param q1 Display first quartile value. Default \code{FALSE}.
+#' @param q3 Display third quartile value. Default \code{FALSE}.
+#' @param iqr Display interquartile range. Default \code{FALSE}.
+#' @param nmiss Display number of missing values. Default \code{FALSE}.
+#' @param nobs Display total number of rows. Default \code{FALSE}.
+#' 
+#' @import dplyr
+#' @import haven
+#' @import purrr
+#' @export
+#' @return A data.frame with columns variable, \code{by} variable, and 
+#'   a column for each summary statistic.
+#'   
+#' @examples
+#' proc_means(iris, vars = c("Sepal.Length", "Sepal.Width"))
+#' proc_means(iris, by = "Species")
+#'
+
+proc_means <- function(df, vars = NULL, by = NULL, n = T, mean = TRUE, 
+                       sd = TRUE, min = TRUE, max = TRUE, median = FALSE,
+                       q1 = FALSE, q3 = FALSE, iqr = FALSE, nmiss = FALSE, 
+                       nobs = FALSE) {
+  
+  # Create quosure of by variable if provided
+  if (!is.null(by)) quo_by <- rlang::sym(by)
+  
+  # If no variables provided, use all variables
+  if (is.null(vars)) vars <- names(df)[!names(df) %in% by]
+
+  # Select variables requested and zap formats and labels from import
+  data <- df %>%
+    when(!is.null(by) ~ select(., one_of(c(vars, by))),
+         ~ select(., one_of(vars))) %>%
+    zap_formats() %>%
+    zap_labels()
+ 
+  # Remove labels if for some reason zap_labels doesn't
+  for (var in colnames(data)) {
+    attr(data[[deparse(as.name(var))]], "label") <- NULL
+  }
+  
+  # Calculate summary statistics and return requested stats
+  data %>%
+    when(!is.null(by) ~ gather(., key = "variable", value = "value", -!!quo_by) %>%
+           group_by(variable, !!quo_by),
+         ~ gather(., key = "variable", value = "value") %>%
+           group_by(variable)
+         ) %>%
+    # Calculate all summary statistics
+    summarize(N = sum(!is.na(value)),
+              Mean = round(mean(value, na.rm = TRUE), 3),
+              SD = round(sd(value, na.rm = TRUE), 3),
+              Min = round(min(value, na.rm = TRUE), 3),
+              Max = round(max(value, na.rm = TRUE), 3),
+              Median = round(median(value, na.rm = TRUE), 3),
+              Q1 = round(quantile(value, 0.25, na.rm = TRUE), 3), 
+              Q3 = round(quantile(value, 0.75, na.rm = TRUE), 3),
+              IQR = round(IQR(value, na.rm = TRUE), 3),
+              NMiss = sum(is.na(value)),
+              NObs = n()) %>%
+    # Select desired statistics to return and display
+    when(!isTRUE(n) ~ select(., -N),
+         ~ select(., everything())) %>%
+    when(!isTRUE(mean) ~ select(., -Mean),
+         ~ select(., everything())) %>%
+    when(!isTRUE(sd) ~ select(., -SD),
+         ~ select(., everything())) %>%
+    when(!isTRUE(min) ~ select(., -Min),
+         ~ select(., everything())) %>%
+    when(!isTRUE(max) ~ select(., -Max),
+         ~ select(., everything())) %>%
+    when(!isTRUE(median) ~ select(., -Median),
+         ~ select(., everything())) %>%
+    when(!isTRUE(q1) ~ select(., -Q1),
+         ~ select(., everything())) %>%
+    when(!isTRUE(q3) ~ select(., -Q3),
+         ~ select(., everything())) %>%
+    when(!isTRUE(iqr) ~ select(., -IQR),
+         ~ select(., everything())) %>%
+    when(!isTRUE(nmiss) ~ select(., -NMiss),
+         ~ select(., everything())) %>%    
+    when(!isTRUE(nobs) ~ select(., -NObs),
+         ~ select(., everything()))
+    
+}
+
+
